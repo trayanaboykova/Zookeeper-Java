9a03a45e5d2b0f0d20469df853db25a22c693c1f
Trayana Boykova <trayana.boykova@gmail.com>
2025-03-07 13:47:38 +0200

    adding project

--- Zookeeper with Java/task/src/Main.java
+++ Zookeeper with Java/task/src/Main.java
@@ -1,5 +1,156 @@
+import java.util.Scanner;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        // Store all habitats in a list
+        List<String> habitats = List.of(
+                """
+                Switching on the camera in the camel habitat...
+                 ___.-''''-.
+                /___  @    |
+                ',,,,.     |         _.'''''''._
+                     '     |        /           \\
+                     |     \\    _.-'             \\
+                     |      '.-'                  '-.
+                     |                               ',
+                     |                                '',
+                      ',,-,                           ':;
+                           ',,| ;,,                 ,' ;;
+                              ! ; !'',,,',',,,,'!  ;   ;:
+                             : ;  ! !       ! ! ;  ;   :;
+                             ; ;   ! !      ! !  ; ;   ;,
+                            ; ;    ! !     ! !   ; ;
+                            ; ;    ! !    ! !     ; ;
+                           ;,,      !,!   !,!     ;,;
+                           /_I      L_I   L_I     /_I
+                Look at that! Our little camel is sunbathing!""",
+
+                """
+                Switching on the camera in the lion habitat...
+                                                               ,w.
+                                                             ,YWMMw  ,M  ,
+                                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                                   _.-""        '''           YP"WMMMMMMMMMb,
+                                .-' __.'                   .'     MMMMW^WMMMM;
+                    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+                 ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\\
+                ,MM:.    .'.-'   .'     ;     `\\    ;     `,     MMMMMMMW `"=./`-,
+                WMMm__,-'.'     /      _.\\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+                "^MP__.-'    ,-' _.--""   `-,   ;       \\  ; ;MMMMMMMMMMW^``; __|
+                           /   .'            ; ;         )  )`{  \\ `"^W^`,   \\  :
+                          /  .'             /  (       .'  /     Ww._     `.  `"
+                         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+                        (--, )                `,_ / `) \\/"")      ^"      `-, -;"\\:
+                The lion is roaring!""",
+
+                """
+                Switching on the camera in the deer habitat...
+                   /|       |\\
+                `__\\       //__'
+                   ||      ||
+                 \\__`\\     |'__/
+                   `_\\   //_'
+                   _.,:---;,._
+                   \\_:     :_/
+                     |@. .@|
+                     |     |
+                     ,\\.-./ \\
+                     ;;`-'   `---__________-----.-.
+                     ;;;                         \\_\\
+                     ';;;                         |
+                      ;    |                      ;
+                       \\   \\     \\        |      /
+                        \\_, \\    /        \\     |\\
+                          |';|  |,,,,,,,,/ \\    \\ \\_
+                          |  |  |           \\   /   |
+                          \\  \\  |           |  / \\  |
+                           | || |           | |   | |
+                           | || |           | |   | |
+                           | || |           | |   | |
+                           |_||_|           |_|   |_|
+                          /_//_/           /_/   /_/
+                Our 'Bambi' looks hungry. Let's go to feed it!""",
+
+                """
+                Switching on the camera in the goose habitat...
+                
+                                                    _
+                                                ,-"" "".
+                                              ,'  ____  `.
+                                            ,'  ,'    `.  `._
+                   (`.         _..--.._   ,'  ,'        \\    \\
+                  (`-.\\    .-""        ""'   /          (  d _b
+                 (`._  `-"" ,._             (            `-(   \\
+                 <_  `     (  <`<            \\              `-._\\
+                  <`-       (__< <           :
+                   (__        (_<_<          ;
+                    `------------------------------------------
+                The goose is staring intently at you... Maybe it's time to change the channel?""",
+
+                """
+                Switching on the camera in the bat habitat...
+                _________________               _________________
+                 ~-.              \\  |\\___/|  /              .-~
+                     ~-.           \\ / o o \\ /           .-~
+                        >           \\  W  //           <
+                       /             /~---~\\             \\
+                      /_            |       |            _\\
+                         ~-.        |       |        .-~
+                            ;        \\     /        i
+                           /___      /\\   /\\      ___\\
+                                ~-. /  \\_/  \\ .-~
+                                   V         V
+                This bat looks like it's doing fine.""",
+
+                """
+                Switching on the camera in the rabbit habitat...
+                         ,
+                        /|      __
+                       / |   ,-~ /
+                      Y :|  //  /
+                      | jj /( .^
+                      >-"~"-v"
+                     /       Y
+                    jo  o    |
+                   ( ~T~     j
+                    >._-' _./
+                   /   "~"  |
+                  Y     _,  |
+                 /| ;-"~ _  l
+                / l/ ,-"~    \\
+                \\//\\/      .- \\
+                 Y        /    Y
+                 l       I     !
+                 ]\\      _\\    /"\\
+                (" ~----( ~   Y.  )
+                It looks like we will soon have more rabbits!"""
+        );
+
+        while (true) {
+            // Prompt user for habitat number or exit
+            System.out.println("Please enter the number of the habitat you would like to view:");
+            String input = scanner.nextLine();
+
+            if (input.equals("exit")) {
+                System.out.println("See you later!");
+                break;
+            }
+
+            try {
+                int habitatNumber = Integer.parseInt(input);
+                if (habitatNumber >= 0 && habitatNumber < habitats.size()) {
+                    System.out.println(habitats.get(habitatNumber));
+                } else {
+                    System.out.println("Invalid habitat number. Please try again.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number or 'exit' to quit.");
+            }
+        }
+
+        scanner.close();
     }
 }
